e7a098130cfd9fca5c4344c50cc14b5a8f2e6674
aditisadwelkar <msamant2017@outlook.com>
2022-07-13 13:33:43 -0700

    Initial Commit

---
+++ CampusFit/CampusFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CampusFitApp.swift
+//  CampusFit
+//
+//  Created by Aditi Sadwelkar on 7/13/22.
+//
+
+import SwiftUI
+
+@main
+struct CampusFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
